7ee9f5bba754e1920dd9b9b076a79d200aabad85
yumura <yumura.s@tsunagu-grp.jp>
2024-08-22 14:33:39 +0900

    feat:set-up

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+
+const MainPage = () => {
+  return (
+    <>
+        <div>Hello</div>
+        <Button>押せ</Button>
+    </>
+  )
+}
+
+export default MainPage
